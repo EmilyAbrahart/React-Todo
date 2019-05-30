acf70ff526dc4209bdb95f21ce5f2410a5e98131
EmilyAbrahart <emilyabrahart@gmail.com>
2019-05-30 13:07:24 +0100

    Task strikethrough on completion

--- src/components/TodoComponents/Todo.jsx
+++ src/components/TodoComponents/Todo.jsx
@@ -2,17 +2,22 @@
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
 
-export default function ToDo({ markComplete, task, id }) {
+export default function ToDo({
+  markComplete, task, id, completed,
+}) {
   const onMarkComplete = (event) => {
     markComplete(id);
   };
 
+  const taskStyle = {
+    textDecoration: completed ? 'line-through' : 'none',
+  };
   return (
-    <p>
-      {task}
+    <div>
+      <span style={taskStyle}>{task}</span>
       <button className="completeBtn" type="submit" onClick={onMarkComplete}>
 				Complete
       </button>
-    </p>
+    </div>
   );
 }
